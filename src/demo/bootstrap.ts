56efe91b5cd12a3c34f547caddc340cd88c6d531
Isaac Mann <isaacplmann@gmail.com>
2017-02-27 10:14:20 -0500

    Pin angular2-hotkeys dependency to 1.x

--- src/demo/bootstrap.ts
+++ src/demo/bootstrap.ts
@@ -1,4 +1,4 @@
-import { DemoModule } from './demo.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { DemoModule } from './demo.module';
 
 platformBrowserDynamic().bootstrapModule(DemoModule);
