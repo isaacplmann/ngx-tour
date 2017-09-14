4a3e3794ffbe363f30c83b93365fcb851d325a7e
kahboom <rachelyordan@gmail.com>
2017-09-14 13:05:53 +0100

    Update public_api and module

--- ngx-tour-ngx-bootstrap/src/public_api.ts
+++ ngx-tour-ngx-bootstrap/src/public_api.ts
@@ -1,2 +1,2 @@
 export * from 'ngx-tour-core';
-export { TourNgBootstrapModule, TourAnchorNgBootstrapDirective, TourStepTemplateComponent } from './ngx-bootstrap.module';
+export { TourNgxBootstrapModule, TourAnchorNgxBootstrapDirective, TourStepTemplateComponent } from './ngx-bootstrap.module';
