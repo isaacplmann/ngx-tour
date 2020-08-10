8c7d5e300619617dbebbfdb3bc81a469e8f7505b
hakimio <sandshrew@gmail.com>
2020-08-10 14:20:38 +0200

    Removed no longer used "ngx-popper" module from the demo app.

--- src/app/ngx-popper/docs.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'docs',
-  templateUrl: './docs.component.html',
-})
-export class DocsComponent {
-}
